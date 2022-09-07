e468a92c4b7675adedd49ecb2d36065b353fe65c
bshf <bungashafa53@gmail.com>
2022-09-07 11:22:04 +0700

    upd pengeluarantruckingheader 9/7

---
+++ app/Http/Controllers/PengeluaranTruckingHeaderController.php
@@ -0,0 +1,192 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Http;
+
+class PengeluaranTruckingHeaderController extends MyController
+{
+    public $title = 'Pengeluaran Trucking';
+    
+    public function index(Request $request)
+    {
+        $title = $this->title;
+        return view('pengeluarantruckingheader.index', compact('title'));
+    }
+
+    public function create()
+    {
+        $title = $this->title;
+
+        $combo = $this->combo();
+
+        return view('pengeluarantruckingheader.add', compact('title','combo'));
+    }
+
+    public function store(Request $request)
+    {
+        try {
+             /* Unformat nominal */
+            $request->nominal = array_map(function ($nominal) {
+                $nominal = str_replace('.', '', $nominal);
+                $nominal = str_replace(',', '', $nominal);
+
+                return $nominal;
+            }, $request->nominal);
+
+            $request->merge([
+                'nominal' => $request->nominal
+            ]);
+
+            $request['modifiedby'] = Auth::user()->name;
+
+            $response = Http::withHeaders($this->httpHeaders)
+                ->withOptions(['verify' => false])
+                ->withToken(session('access_token'))
+                ->post(config('app.api_url') . 'pengeluarantruckingheader', $request->all());
+
+
+            return response($response, $response->status());
+        } catch (\Throwable $th) {
+            throw $th->getMessage();
+        }
+    }
+
+    public function get($params = [])
+    {
+        $params = [
+            'offset' => $params['offset'] ?? request()->offset ?? ((request()->page - 1) * request()->rows),
+            'limit' => $params['rows'] ?? request()->rows ?? 0,
+            'sortIndex' => $params['sidx'] ?? request()->sidx,
+            'sortOrder' => $params['sord'] ?? request()->sord,
+            'search' => json_decode($params['filters'] ?? request()->filters, 1) ?? [],
+            'withRelations' => $params['withRelations'] ?? request()->withRelations ?? false,
+        ];
+
+        $response = Http::withHeaders(request()->header())
+            ->withOptions(['verify' => false])
+            ->withToken(session('access_token'))
+            ->get(config('app.api_url') . 'pengeluarantruckingheader', $params);
+
+        $data = [
+            'total' => $response['attributes']['totalPages'] ?? [],
+            'records' => $response['attributes']['totalRows'] ?? [],
+            'rows' => $response['data'] ?? [],
+            'params' => $response['params'] ?? [],
+        ];
+
+        return $data;
+    }
+
+    public function edit($id)
+    {
+        $title = $this->title;
+
+        $response = Http::withHeaders($this->httpHeaders)
+            ->withOptions(['verify' => false])
+            ->withToken(session('access_token'))
+            ->get(config('app.api_url') . "pengeluarantruckingheader/$id");
+            // dd($response->getBody()->getContents());
+
+        $pengeluarantruckingheader = $response['data'];
+        $kode = $response['kode'];
+
+        if($kode == 'PJT'){
+            $pengeluarantruckingheaderNoBukti = $this->getNoBukti('PINJAMAN SUPIR', 'PINJAMAN SUPIR', 'pengeluarantruckingheader');
+        }else{
+            $pengeluarantruckingheaderNoBukti = $this->getNoBukti('BIAYA LAIN SUPIR', 'BIAYA LAIN SUPIR', 'pengeluarantruckingheader');
+        }
+
+
+        $combo = $this->combo();
+
+        return view('pengeluarantruckingheader.edit', compact('title', 'pengeluarantruckingheader','combo', 'pengeluarantruckingheaderNoBukti'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        /* Unformat nominal */
+        $request->nominal = array_map(function ($nominal) {
+            $nominal = str_replace('.', '', $nominal);
+            $nominal = str_replace(',', '', $nominal);
+
+            return $nominal;
+        }, $request->nominal);
+
+        $request->merge([
+            'nominal' => $request->nominal
+        ]);
+
+        $request['modifiedby'] = Auth::user()->name;
+
+        $response = Http::withHeaders($this->httpHeaders)
+            ->withOptions(['verify' => false])
+            ->withToken(session('access_token'))
+            ->patch(config('app.api_url') . "pengeluarantruckingheader/$id", $request->all());
+
+        return response($response);
+    }
+
+    public function delete($id)
+    {
+        try {
+            $title = $this->title;
+
+            $response = Http::withHeaders($this->httpHeaders)
+                ->withOptions(['verify' => false])
+                ->withToken(session('access_token'))
+                ->get(config('app.api_url') . "pengeluarantruckingheader/$id");
+
+            $pengeluarantruckingheader = $response['data'];
+            
+            $combo = $this->combo();
+
+            return view('pengeluarantruckingheader.delete', compact('title','combo', 'pengeluarantruckingheader'));
+        } catch (\Throwable $th) {
+            return redirect()->route('pengeluarantruckingheader.index');
+        }
+    }
+
+    public function destroy($id)
+    {
+        $request['modifiedby'] = Auth::user()->name;
+        $response = Http::withHeaders($this->httpHeaders)
+            ->withOptions(['verify' => false])
+            ->withToken(session('access_token'))
+            ->delete(config('app.api_url') . "pengeluarantruckingheader/$id");
+
+            
+        return response($response);
+    }
+
+    public function getNoBukti($group, $subgroup, $table)
+    {
+        $params = [
+            'group' => $group,
+            'subgroup' => $subgroup,
+            'table' => $table
+        ];
+
+        $response = Http::withHeaders($this->httpHeaders)
+            ->withOptions(['verify' => false])
+            ->withToken(session('access_token'))
+            ->get(config('app.api_url') . "running_number", $params);
+
+        $noBukti = $response['data'] ?? 'No bukti tidak ditemukan';
+
+        return $noBukti;
+    }
+
+    private function combo()
+    {
+        $response = Http::withHeaders($this->httpHeaders)
+        ->withToken(session('access_token'))
+        ->withOptions(['verify' => false])
+            ->get(config('app.api_url') . 'pengeluarantruckingheader/combo');
+
+        return $response['data'];
+    }
+
+}
--- resources/views/pengeluarantruckingheader/index.blade.php
+++ resources/views/pengeluarantruckingheader/index.blade.php
@@ -6,7 +6,7 @@
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="rangeModalLabel">Pilih baris</h5>
+        <h5 class="modal-title" id="rangeModalLabel">Pilih bariss</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
@@ -409,7 +409,7 @@
       window.open(`${actionUrl}?${$('#formRange').serialize()}&${params}`)
     })
   })
-
+ 
 </script>
 @endpush()
 @endsection
