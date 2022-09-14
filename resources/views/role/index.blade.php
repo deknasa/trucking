7de766169004354c86040e5c988faf9de38c58f4
Hermes <hermestaspusat@gmail.com>
2022-09-14 15:35:29 +0700

    modal implementation on role

--- resources/views/role/index.blade.php
+++ resources/views/role/index.blade.php
@@ -6,35 +6,14 @@
   <div class="row">
     <div class="col-12">
       <table id="jqGrid"></table>
-      <div id="jqGridPager" class="row bg-white">
-        <div id="buttonContainer" class="col-12 col-md-7 text-center text-md-left">
-          <button id="add" class="btn btn-primary btn-sm mb-1">
-            <i class="fa fa-plus"></i> ADD
-          </button>
-          <button id="edit" class="btn btn-success btn-sm mb-1">
-            <i class="fa fa-pen"></i> EDIT
-          </button>
-          <button id="delete" class="btn btn-danger btn-sm mb-1">
-            <i class="fa fa-trash"></i> DELETE
-          </button>
-          <button id="export" class="btn btn-warning btn-sm mb-1">
-            <i class="fa fa-file-export"></i> EXPORT
-          </button>
-          <button id="report" class="btn btn-info btn-sm mb-1">
-            <i class="fa fa-print"></i> REPORT
-          </button>
-        </div>
-        <div id="pagerHandler" class="col-12 col-md-4 d-flex justify-content-center align-items-center"></div>
-        <div id="pagerInfo" class="col-12 col-md-1 d-flex justify-content-end align-items-center"></div>
-      </div>
     </div>
   </div>
 </div>
 
+@include('role._modal')
+
 @push('scripts')
 <script>
-  let indexUrl = "{{ route('role.index') }}"
-  let getUrl = "{{ route('role.get') }}"
   let indexRow = 0;
   let page = 0;
   let pager = '#jqGridPager'
@@ -50,34 +29,6 @@
   let autoNumericElements = []
 
   $(document).ready(function() {
-    popup = "<?= @$_GET['popup'] ?>" == "" ? "" : "ada";
-    id = "<?= @$_GET['name'] ?>" == "" ? "undefined" : "<?= @$_GET['name'] ?>";
-
-    /* Set page */
-    <?php if (isset($_GET['page'])) { ?>
-      page = "{{ $_GET['page'] }}"
-    <?php } ?>
-
-    /* Set id */
-    <?php if (isset($_GET['id'])) { ?>
-      id = "{{ $_GET['id'] }}"
-    <?php } ?>
-
-    /* Set indexRow */
-    <?php if (isset($_GET['indexRow'])) { ?>
-      indexRow = "{{ $_GET['indexRow'] }}"
-    <?php } ?>
-
-    /* Set sortname */
-    <?php if (isset($_GET['sortname'])) { ?>
-      sortname = "{{ $_GET['sortname'] }}"
-    <?php } ?>
-
-    /* Set sortorder */
-    <?php if (isset($_GET['sortorder'])) { ?>
-      sortorder = "{{ $_GET['sortorder'] }}"
-    <?php } ?>
-
     $("#jqGrid").jqGrid({
         url: `{{ config('app.api_url') . 'role' }}`,
         mtype: "GET",
@@ -139,38 +90,16 @@
           records: 'attributes.totalRows',
         },
         onSelectRow: function(id) {
-          id = $(this).jqGrid('getCell', id, 'rn') - 1
-          indexRow = id
+          activeGrid = $(this)
+          indexRow = $(this).jqGrid('getCell', id, 'rn') - 1
           page = $(this).jqGrid('getGridParam', 'page')
           let rows = $(this).jqGrid('getGridParam', 'postData').limit
           if (indexRow >= rows) indexRow = (indexRow - rows * (page - 1))
         },
-        ondblClickRow: function(rowid) {
-          if (popup == "ada") {
-            var rowData = jQuery(this).getRowData(rowid);
-            localStorage.setItem('getRole_id', JSON.stringify(rowData));
-            window.close();
-          }
-        },
         loadBeforeSend: (jqXHR) => {
-          jqXHR.setRequestHeader('Authorization', `Bearer {{ session('access_token') }}`)
-        },
-        beforeRequest: function() {
-          var $requestGrid = $(this);
-          if ($requestGrid.data('areFiltersDefaulted') !== true) {
-            $requestGrid.data('areFiltersDefaulted', true);
-            setTimeout(function() {
-              $requestGrid[0].triggerToolbar();
-            }, 50);
-            return false;
-          }
-          // Subsequent runs are always allowed
-          return true;
+          jqXHR.setRequestHeader('Authorization', `Bearer ${accessToken}`)
         },
         loadComplete: function(data) {
-          loadPagerHandler('#pagerHandler', $(this))
-          loadPagerInfo('#pagerInfo', $(this))
-
           $(document).unbind('keydown')
           setCustomBindKeys($(this))
           initResize($(this))
@@ -181,6 +110,7 @@
           totalRecord = $(this).getGridParam("records")
           limit = $(this).jqGrid('getGridParam', 'postData').limit
           postData = $(this).jqGrid('getGridParam', 'postData')
+          triggerClick = true
 
           $('.clearsearchclass').click(function() {
             clearColumnSearch()
@@ -221,6 +151,58 @@
         beforeSearch: function() {
           clearGlobalSearch()
         },
+      })
+
+      .customPager({
+        buttons: [{
+            id: 'add',
+            innerHTML: '<i class="fa fa-plus"></i> ADD',
+            class: 'btn btn-primary btn-sm mr-1',
+            onClick: () => {
+              createRole()
+            }
+          },
+          {
+            id: 'edit',
+            innerHTML: '<i class="fa fa-pen"></i> EDIT',
+            class: 'btn btn-success btn-sm mr-1',
+            onClick: () => {
+              selectedId = $("#jqGrid").jqGrid('getGridParam', 'selrow')
+
+              editRole(selectedId)
+            }
+          },
+          {
+            id: 'delete',
+            innerHTML: '<i class="fa fa-trash"></i> DELETE',
+            class: 'btn btn-danger btn-sm mr-1',
+            onClick: () => {
+              selectedId = $("#jqGrid").jqGrid('getGridParam', 'selrow')
+
+              deleteRole(selectedId)
+            }
+          },
+          {
+            id: 'export',
+            innerHTML: '<i class="fa fa-file-export"></i> EXPORT',
+            class: 'btn btn-warning btn-sm mr-1',
+            onClick: () => {
+              $('#rangeModal').data('action', 'export')
+              $('#rangeModal').find('button:submit').html(`Export`)
+              $('#rangeModal').modal('show')
+            }
+          },
+          {
+            id: 'report',
+            innerHTML: '<i class="fa fa-print"></i> REPORT',
+            class: 'btn btn-info btn-sm mr-1',
+            onClick: () => {
+              $('#rangeModal').data('action', 'report')
+              $('#rangeModal').find('button:submit').html(`Report`)
+              $('#rangeModal').modal('show')
+            }
+          },
+        ]
       })
 
     /* Append clear filter button */
@@ -342,41 +324,6 @@
 
         submitButton.removeAttr('disabled')
       }
-    })
-
-    /* Handle button add on click */
-    $('#add').click(function() {
-      let limit = $("#jqGrid").jqGrid('getGridParam', 'postData').limit
-
-      window.location.href = `{{ route('role.create') }}?sortname=${sortname}&sortorder=${sortorder}&limit=${limit}`
-    })
-
-    /* Handle button edit on click */
-    $('#edit').click(function() {
-      selectedId = $("#jqGrid").jqGrid('getGridParam', 'selrow')
-
-      window.location.href = `${indexUrl}/${selectedId}/edit?sortname=${sortname}&sortorder=${sortorder}&limit=${limit}`
-    })
-
-    /* Handle button delete on click */
-    $('#delete').click(function() {
-      selectedId = $("#jqGrid").jqGrid('getGridParam', 'selrow')
-
-      window.location.href = `${indexUrl}/${selectedId}/delete?sortname=${sortname}&sortorder=${sortorder}&limit=${limit}&page=${page}&indexRow=${indexRow}`
-    })
-
-    /* Handle button export on click */
-    $('#export').click(function() {
-      $('#rangeModal').data('action', 'export')
-      $('#rangeModal').find('button:submit').html(`Export`)
-      $('#rangeModal').modal('show')
-    })
-
-    /* Handle button report on click */
-    $('#report').click(function() {
-      $('#rangeModal').data('action', 'report')
-      $('#rangeModal').find('button:submit').html(`Report`)
-      $('#rangeModal').modal('show')
     })
   })
 </script>
