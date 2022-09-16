3f8cdeeddc56a2e767a64a89b2b5c015791ccf32
Hermes <hermestaspusat@gmail.com>
2022-09-16 11:47:13 +0700

    modal implementation on pelanggan

---
+++ resources/views/pelanggan/_modal.blade.php
@@ -0,0 +1,333 @@
+<div class="modal fade modal-fullscreen" id="crudModal" tabindex="-1" aria-labelledby="crudModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <form action="#" id="crudForm">
+      <div class="modal-content">
+        <div class="modal-header bg-primary">
+          <h5 class="modal-title" id="crudModalTitle"></h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <form action="" method="post">
+          <div class="modal-body">
+
+            <div class="row form-group">
+              <div class="col-12 col-sm-3 col-md-2 col-form-label">
+                <label>ID</label>
+              </div>
+              <div class="col-12 col-sm-9 col-md-10">
+                <input type="text" name="id" class="form-control" readonly>
+              </div>
+            </div>
+            <div class="row form-group">
+              <div class="col-12 col-sm-3 col-md-2 col-form-label">
+                <label>
+                  kode pelanggan <span class="text-danger">*</span>
+                </label>
+              </div>
+              <div class="col-12 col-sm-9 col-md-10">
+                <input type="text" name="kodepelanggan" class="form-control">
+              </div>
+            </div>
+            <div class="row form-group">
+              <div class="col-12 col-sm-3 col-md-2 col-form-label">
+                <label>
+                  nama pelanggan <span class="text-danger">*</span>
+                </label>
+              </div>
+              <div class="col-12 col-sm-9 col-md-10">
+                <input type="text" name="namapelanggan" class="form-control">
+              </div>
+            </div>
+            <div class="row form-group">
+              <div class="col-12 col-sm-3 col-md-2 col-form-label">
+                <label>
+                  telp <span class="text-danger">*</span>
+                </label>
+              </div>
+              <div class="col-12 col-sm-9 col-md-10">
+                <input type="text" name="telp" class="form-control">
+              </div>
+            </div>
+            <div class="row form-group">
+              <div class="col-12 col-sm-3 col-md-2 col-form-label">
+                <label>
+                  alamat <span class="text-danger">*</span>
+                </label>
+              </div>
+              <div class="col-12 col-sm-9 col-md-10">
+                <input type="text" name="alamat" class="form-control">
+              </div>
+            </div>
+            <div class="row form-group">
+              <div class="col-12 col-sm-3 col-md-2 col-form-label">
+                <label>
+                  alamat2 <span class="text-danger">*</span>
+                </label>
+              </div>
+              <div class="col-12 col-sm-9 col-md-10">
+                <input type="text" name="alamat2" class="form-control">
+              </div>
+            </div>
+            <div class="row form-group">
+              <div class="col-12 col-sm-3 col-md-2 col-form-label">
+                <label>
+                  kota <span class="text-danger">*</span>
+                </label>
+              </div>
+              <div class="col-12 col-sm-9 col-md-10">
+                <input type="text" name="kota" class="form-control">
+              </div>
+            </div>
+            <div class="row form-group">
+              <div class="col-12 col-sm-3 col-md-2 col-form-label">
+                <label>
+                  kode pos <span class="text-danger">*</span>
+                </label>
+              </div>
+              <div class="col-12 col-sm-9 col-md-10">
+                <input type="text" name="kodepos" class="form-control">
+              </div>
+            </div>
+            <div class="row form-group">
+              <div class="col-12 col-sm-3 col-md-2 col-form-label">
+                <label>
+                  keterangan <span class="text-danger">*</span>
+                </label>
+              </div>
+              <div class="col-12 col-sm-9 col-md-10">
+                <input type="text" name="keterangan" class="form-control">
+              </div>
+            </div>
+          </div>
+          <div class="modal-footer justify-content-start">
+            <button id="btnSubmit" class="btn btn-primary">
+              <i class="fa fa-save"></i>
+              Simpan
+            </button>
+            <button class="btn btn-secondary" data-dismiss="modal">
+              <i class="fa fa-times"></i>
+              Batal
+            </button>
+          </div>
+        </form>
+      </div>
+    </form>
+  </div>
+</div>
+
+@push('scripts')
+<script>
+  let hasFormBindKeys = false
+
+  $(document).ready(function() {
+    $('#btnSubmit').click(function(event) {
+      event.preventDefault()
+
+      let method
+      let url
+      let form = $('#crudForm')
+      let pelangganId = form.find('[name=id]').val()
+      let action = form.data('action')
+      let data = $('#crudForm').serializeArray()
+
+      data.push({
+        name: 'sortIndex',
+        value: $('#jqGrid').getGridParam().sortname
+      })
+      data.push({
+        name: 'sortOrder',
+        value: $('#jqGrid').getGridParam().sortorder
+      })
+      data.push({
+        name: 'filters',
+        value: $('#jqGrid').getGridParam('postData').filters
+      })
+      data.push({
+        name: 'indexRow',
+        value: indexRow
+      })
+      data.push({
+        name: 'page',
+        value: page
+      })
+      data.push({
+        name: 'limit',
+        value: limit
+      })
+
+      switch (action) {
+        case 'add':
+          method = 'POST'
+          url = `${apiUrl}pelanggan`
+          break;
+        case 'edit':
+          method = 'PATCH'
+          url = `${apiUrl}pelanggan/${pelangganId}`
+          break;
+        case 'delete':
+          method = 'DELETE'
+          url = `${apiUrl}pelanggan/${pelangganId}`
+          break;
+        default:
+          method = 'POST'
+          url = `${apiUrl}pelanggan`
+          break;
+      }
+
+      $(this).attr('disabled', '')
+      $('#loader').removeClass('d-none')
+
+      $.ajax({
+        url: url,
+        method: method,
+        dataType: 'JSON',
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        },
+        data: data,
+        success: response => {
+          $('#crudForm').trigger('reset')
+          $('#crudModal').modal('hide')
+
+          id = response.data.id
+
+          $('#jqGrid').trigger('reloadGrid', {
+            page: response.data.page
+          })
+
+          if (response.data.grp == 'FORMAT') {
+            updateFormat(response.data)
+          }
+        },
+        error: error => {
+          if (error.status === 422) {
+            $('.is-invalid').removeClass('is-invalid')
+            $('.invalid-feedback').remove()
+
+            setErrorMessages(form, error.responseJSON.errors);
+          } else {
+            showDialog(error.statusText)
+          }
+        },
+      }).always(() => {
+        $('#loader').addClass('d-none')
+        $(this).removeAttr('disabled')
+      })
+    })
+  })
+
+  $('#crudModal').on('shown.bs.modal', () => {
+    let form = $('#crudForm')
+
+    setFormBindKeys(form)
+
+    activeGrid = null
+
+    getMaxLength(form)
+  })
+
+  $('#crudModal').on('hidden.bs.modal', () => {
+    activeGrid = '#jqGrid'
+  })
+
+  function createPelanggan() {
+    let form = $('#crudForm')
+
+    form.trigger('reset')
+    form.find('#btnSubmit').html(`
+    <i class="fa fa-save"></i>
+    Simpan
+  `)
+    form.data('action', 'add')
+    form.find(`.sometimes`).show()
+    $('#crudModalTitle').text('Create Pelanggan')
+    $('#crudModal').modal('show')
+    $('.is-invalid').removeClass('is-invalid')
+    $('.invalid-feedback').remove()
+  }
+
+  function editPelanggan(pelangganId) {
+    let form = $('#crudForm')
+
+    form.data('action', 'edit')
+    form.trigger('reset')
+    form.find('#btnSubmit').html(`
+    <i class="fa fa-save"></i>
+    Simpan
+  `)
+    form.find(`.sometimes`).hide()
+    $('#crudModalTitle').text('Edit Pelanggan')
+    $('#crudModal').modal('show')
+    $('.is-invalid').removeClass('is-invalid')
+    $('.invalid-feedback').remove()
+
+    showPelanggan(form, pelangganId)
+  }
+
+  function deletePelanggan(pelangganId) {
+    let form = $('#crudForm')
+
+    form.data('action', 'delete')
+    form.trigger('reset')
+    form.find('#btnSubmit').html(`
+    <i class="fa fa-save"></i>
+    Hapus
+  `)
+    form.find(`.sometimes`).hide()
+    $('#crudModalTitle').text('Delete Pelanggan')
+    $('#crudModal').modal('show')
+    $('.is-invalid').removeClass('is-invalid')
+    $('.invalid-feedback').remove()
+
+    showPelanggan(form, pelangganId)
+  }
+
+  function getMaxLength(form) {
+    if (!form.attr('has-maxlength')) {
+      $.ajax({
+        url: `${apiUrl}pelanggan/field_length`,
+        method: 'GET',
+        dataType: 'JSON',
+        headers: {
+          'Authorization': `Bearer ${accessToken}`
+        },
+        success: response => {
+          $.each(response.data, (index, value) => {
+            if (value !== null && value !== 0 && value !== undefined) {
+              form.find(`[name=${index}]`).attr('maxlength', value)
+            }
+          })
+
+          form.attr('has-maxlength', true)
+        },
+        error: error => {
+          showDialog(error.statusText)
+        }
+      })
+    }
+  }
+
+  function showPelanggan(form, pelangganId) {
+    $.ajax({
+      url: `${apiUrl}pelanggan/${pelangganId}`,
+      method: 'GET',
+      dataType: 'JSON',
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      },
+      success: response => {
+        $.each(response.data, (index, value) => {
+          let element = form.find(`[name="${index}"]`)
+
+          if (element.is('select')) {
+            element.val(value).trigger('change')
+          } else {
+            element.val(value)
+          }
+        })
+      }
+    })
+  }
+</script>
+@endpush()
