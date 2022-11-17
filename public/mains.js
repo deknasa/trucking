0d831d9a97bc53b3258bde4038d8571ce8e68784
Hermes <hermestaspusat@gmail.com>
2022-11-17 09:21:45 +0700

    fix stack modal scroll issue

--- public/mains.js
+++ public/mains.js
@@ -21,6 +21,21 @@ $(document).ready(function () {
 
 		sidebarIsOpen = false;
 	});
+
+	$(document).on("show.bs.modal", ".modal", function () {
+		const zIndex = 1040 + 10 * $(".modal:visible").length;
+		$(this).css("z-index", zIndex);
+		setTimeout(() =>
+			$(".modal-backdrop")
+				.not(".modal-stack")
+				.css("z-index", zIndex - 1)
+				.addClass("modal-stack")
+		);
+	});
+
+	$(document).on("hidden.bs.modal", ".modal", function () {
+		$(".modal:visible").length && $(document.body).addClass("modal-open");
+	});
 });
 
 function setFormats() {
