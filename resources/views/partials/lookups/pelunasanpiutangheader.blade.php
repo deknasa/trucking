194de6bba35cee2b34440900eae0db7dd4792488
iqbalrafli13 <iqbal13rafli@gmail.com>
2022-12-07 14:45:04 +0700

    invoiceExtra print

--- resources/views/partials/lookups/pelunasanpiutangheader.blade.php
+++ resources/views/partials/lookups/pelunasanpiutangheader.blade.php
@@ -55,19 +55,9 @@
             align: 'left'
           },
           {
-            label: 'CREATEDAT',
-            name: 'created_at',
-            align: 'right',
-            formatter: "date",
-            formatoptions: {
-              srcformat: "ISO8601Long",
-              newformat: "d-m-Y H:i:s"
-            }
-          },
-          {
             label: 'UPDATEDAT',
             name: 'updated_at',
-            align: 'right',
+            align: 'left',
             formatter: "date",
             formatoptions: {
               srcformat: "ISO8601Long",
