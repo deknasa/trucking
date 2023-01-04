72726bc6b828811e3862e6adfff1c6d15e5e9701
iqbalrafli13 <iqbal13rafli@gmail.com>
2023-01-04 11:08:20 +0700

    tutup buku

--- app/Libraries/Myauth.php
+++ app/Libraries/Myauth.php
@@ -19,7 +19,7 @@ class Myauth
             '',
             'dashboard',
             'logtrail',
-            'kartustok',
+            'tutupbuku',
             'historipenerimaanstok',
             'historipengeluaranstok',
             'gudang'
