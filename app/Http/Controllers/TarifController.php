23a44ff2cd7ce10788b310a956b88c50f48e0e98
bshf <bungashafa53@gmail.com>
2023-02-02 15:36:26 +0700

    create import tarif

--- app/Http/Controllers/TarifController.php
+++ app/Http/Controllers/TarifController.php
@@ -9,6 +9,8 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Auth;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 class TarifController extends MyController
 {
@@ -20,15 +22,13 @@ class TarifController extends MyController
     public function index(Request $request)
     {
         $title = $this->title;
-        $breadcrumb = $this->breadcrumb;
         $data = [
             'combo' => $this->comboStatusAktif('list'),
             'comboton' => $this->combocetak('list', 'SISTEM TON', 'SISTEM TON'),
             'combopenyesuaianharga' => $this->combocetak('list', 'PENYESUAIAN HARGA', 'PENYESUAIAN HARGA'),
         ];
 
-        return view('tarif.index', compact('title', 'breadcrumb', 'data'));        
-
+        return view('tarif.index', compact('title', 'data'));
     }
 
     /**
@@ -71,7 +71,7 @@ class TarifController extends MyController
         $title = $this->title;
         $combo = $this->combo();
 
-        return view('tarif.add', compact('title','combo'));
+        return view('tarif.add', compact('title', 'combo'));
     }
 
     /**
@@ -114,7 +114,7 @@ class TarifController extends MyController
 
         $tarif = $response['data'];
 
-        return view('tarif.edit', compact('title', 'tarif','combo'));
+        return view('tarif.edit', compact('title', 'tarif', 'combo'));
     }
 
     /**
@@ -128,7 +128,7 @@ class TarifController extends MyController
             'nominal' => str_replace(',', '', str_replace('.', '', $request->nominal)),
             'nominalton' => str_replace(',', '', str_replace('.', '', $request->nominalton)),
         ]);
-        
+
         $response = Http::withHeaders([
             'Accept' => 'application/json',
             'Content-Type' => 'application/json',
@@ -157,7 +157,7 @@ class TarifController extends MyController
 
             $tarif = $response['data'];
 
-            return view('tarif.delete', compact('title', 'tarif','combo'));
+            return view('tarif.delete', compact('title', 'tarif', 'combo'));
         } catch (\Throwable $th) {
             return redirect()->route('tarif.index');
         }
@@ -194,7 +194,7 @@ class TarifController extends MyController
     {
         $response = Http::withHeaders($this->httpHeaders)->withOptions(['verify' => false])->withToken(session('access_token'))
             ->get(config('app.api_url') . 'tarif/combo');
-        
+
         return $response['data'];
     }
 
@@ -231,4 +231,67 @@ class TarifController extends MyController
         return $response['data'];
     }
 
+    public function export(Request $request): void
+    {
+
+        $tarif = Http::withHeaders($request->header())
+        ->withOptions(['verify' => false])
+        ->withToken(session('access_token'))
+        ->get(config('app.api_url') . 'tarif/listpivot')['data'];
+        
+        foreach($tarif[0] as $key => $value)
+        {
+            dd($key);
+        }
+
+        $spreadsheet = new Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet();
+        $sheet->setCellValue('A1', 'TAS TARIF');
+        $sheet->getStyle("A1")->getFont()->setSize(20);
+        $sheet->getStyle('A1')->getAlignment()->setHorizontal('center');
+        $sheet->mergeCells('A1:G1');
+
+        $header_start_row = 2;
+        $detail_table_header_row = 7;
+        $detail_start_row = $detail_table_header_row + 1;
+
+        $header_columns = [
+            [
+                'label' => 'No Bukti',
+                'index' => 'nobukti',
+            ],
+            [
+                'label' => 'Tanggal',
+                'index' => 'tglbukti',
+            ],
+            [
+                'label' => 'No Bukti Invoice',
+                'index' => 'invoice_nobukti',
+            ],
+        ];
+
+        $detail_columns = [
+            [
+                'label' => 'No',
+            ],
+            [
+                'label' => 'No Bukti',
+                'index' => 'nobukti',
+            ],
+            [
+                'label' => 'Keterangan',
+                'index' => 'keterangan',
+            ],
+            [
+                'label' => 'Nominal',
+                'index' => 'nominal',
+                'format' => 'currency'
+            ]
+        ];
+
+        foreach ($header_columns as $header_column) {
+            $sheet->setCellValue('B' . $header_start_row, $header_column['label']);
+            $sheet->setCellValue('C' . $header_start_row++, ': ');
+        }
+    }
 }
