9825de9c676f49444120695eaa17814a4380eaff
syarifa20 <syaripah2004@gmail.com>
2023-05-09 10:39:28 +0700

    add lap kartu hutang per vendor and lap warkat belum cair

---
+++ resources/views/laporanhutangpervendordetail/index.blade.php
@@ -0,0 +1,163 @@
+@extends('layouts.master')
+
+@section('content')
+<!-- Grid -->
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12">
+            <div class="card card-primary">
+                <div class="card-header">
+                </div>
+                <form id="crudForm">
+                    <div class="card-body">
+                        <div class="form-group row">
+                            <label class="col-12 col-sm-2 col-form-label mt-2">Periode<span class="text-danger">*</span></label>
+                            <div class="col-sm-4 mt-2">
+                                <div class="input-group">
+                                    <input type="text" name="dari" class="form-control datepicker">
+                                </div>
+                            </div>
+                            <h5 class="mt-3">s/d</h5>
+                            <div class="col-sm-4 mt-2">
+                                <div class="input-group">
+                                    <input type="text" name="sampai" class="form-control datepicker">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <label class="col-12 col-sm-2 col-form-label mt-2">VENDOR<span class="text-danger">*</span></label>
+                            <div class="col-sm-4 mt-2">
+                                <input type="hidden" name="supplierdari_id">
+                                <input type="text" name="supplierdari" class="form-control supplierdari-lookup">
+                            </div>
+                            <h5 class="mt-3">s/d</h5>
+                            <div class="col-sm-4 mt-2">
+                                <input type="hidden" name="suppliersampai_id">
+                                <input type="text" name="suppliersampai" class="form-control suppliersampai-lookup">
+                            </div>
+                        </div>
+                       
+                        <div class="row">
+
+                            <div class="col-sm-6 mt-4">
+                                <a id="btnPreview" class="btn btn-secondary mr-2 ">
+                                    <i class="fas fa-sync"></i>
+                                    Cetak
+                                </a>
+                            </div>
+                        </div>
+
+                    </div>
+                </form>
+            </div>
+            <table id="jqGrid"></table>
+        </div>
+    </div>
+</div>
+
+@push('scripts')
+<script>
+    let indexRow = 0;
+    let page = 0;
+    let pager = '#jqGridPager'
+    let popup = "";
+    let id = "";
+    let triggerClick = true;
+    let highlightSearch;
+    let totalRecord
+    let limit
+    let postData
+    let sortname = 'nobukti'
+    let sortorder = 'asc'
+    let autoNumericElements = []
+    let rowNum = 10
+    let hasDetail = false
+
+
+    $(document).ready(function() {
+        initLookup()
+
+        initDatepicker()
+        $('#crudForm').find('[name=dari]').val($.datepicker.formatDate('dd-mm-yy', new Date())).trigger('change');
+        $('#crudForm').find('[name=sampai]').val($.datepicker.formatDate('dd-mm-yy', new Date())).trigger('change');
+
+
+        let css_property =
+        {
+            "color": "#fff",
+            "background-color": "rgb(173 180 187)",
+            "cursor" : "not-allowed",
+            "border-color": "rgb(173 180 187)"
+        }
+        if (!`{{ $myAuth->hasPermission('lapkartuhutangpervendordetail', 'report') }}`) {
+            $('#btnPreview').prop('disabled', true)
+            $('#btnPreview').css(css_property);
+        }
+    })
+
+    $(document).on('click', `#btnPreview`, function(event) {
+        let sampai = $('#crudForm').find('[name=sampai]').val()
+        let dari = $('#crudForm').find('[name=dari]').val()
+        let supplierDari = $('#crudForm').find('[name=supplierdari_id]').val()
+        let supplierSampai= $('#crudForm').find('[name=suppliersampai_id]').val()
+
+        if (dari != '' && sampai != '' && supplierDari!= ''  && supplierSampai!= '') {
+
+            window.open(`{{ route('lapkartuhutangpervendordetail.report') }}?sampai=${sampai}&dari=${dari}&supplierdari=${supplierDari}&suppliersampai=${supplierSampai}`)
+        } else {
+            showDialog('ISI SELURUH KOLOM')
+        }
+    })
+
+
+    function initLookup()
+    {
+        $('.supplierdari-lookup').lookup({
+            title: 'supplier dari Lookup',
+            fileName: 'supplier',
+            beforeProcess: function(test) {
+                this.postData = {
+                    Aktif: 'AKTIF',
+                }
+            },
+            onSelectRow: (supplier, element) => {
+                $('#crudForm [name=supplierdari_id]').first().val(supplier.id)
+                element.val(supplier.namasupplier)
+                element.data('currentValue', element.val())
+            },
+            onCancel: (element) => {
+                element.val(element.data('currentValue'))
+            },
+            onClear: (element) => {
+                element.val('')
+                $(`#crudForm [name="supplierdari_id"]`).first().val('')
+                element.data('currentValue', element.val())
+            }
+        })
+
+        $('.suppliersampai-lookup').lookup({
+            title: 'supplier sampai Lookup',
+            fileName: 'supplier',
+            beforeProcess: function(test) {
+                this.postData = {
+                    Aktif: 'AKTIF',
+                }
+            },
+            onSelectRow: (supplier, element) => {
+                $('#crudForm [name=suppliersampai_id]').first().val(supplier.id)
+                element.val(supplier.namasupplier)
+                element.data('currentValue', element.val())
+            },
+            onCancel: (element) => {
+                element.val(element.data('currentValue'))
+            },
+            onClear: (element) => {
+                element.val('')
+                $(`#crudForm [name="suppliersampai_id"]`).first().val('')
+                element.data('currentValue', element.val())
+            }
+        })
+    }
+</script>
+@endpush()
+@endsection
