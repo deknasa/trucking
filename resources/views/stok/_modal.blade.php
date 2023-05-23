dc929c809be6068e30552b492383165bbe75b3cc
syarifa20 <syaripah2004@gmail.com>
2023-05-23 13:36:32 +0700

    fix keterangan menjadi tidak wajib diisi

--- resources/views/stok/_modal.blade.php
+++ resources/views/stok/_modal.blade.php
@@ -91,7 +91,7 @@
             <div class="row form-group">
 
               <div class="col-12 col-sm-3 col-md-2">
-                <label class="col-form-label">keterangan <span class="text-danger">*</span> </label>
+                <label class="col-form-label">keterangan</label>
               </div>
               <div class="col-12 col-sm-9 col-md-10">
                 <input type="text" name="keterangan" class="form-control">
