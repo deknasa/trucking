391d331ef7c38c308aeca5f9bf221ae27047e94e
syarifa20 <syaripah2004@gmail.com>
2023-05-24 17:39:17 +0700

    add report export mandor

--- app/Http/Controllers/MandorController.php
+++ app/Http/Controllers/MandorController.php
@@ -203,4 +203,35 @@ class MandorController extends MyController
 
         return $response['data'];
     }
+    public function report(Request $request)
+    {
+        $response = Http::withHeaders($this->httpHeaders)
+            ->withOptions(['verify' => false])
+            ->withToken(session('access_token'))
+            ->get(config('app.api_url') . 'mandor', $request->all());
+
+        $mandors = $response['data'];
+
+
+        $i = 0;
+        foreach ($mandors as $index => $params) {
+
+            $statusaktif = $params['statusaktif'];
+
+            $result = json_decode($statusaktif, true);
+
+            $statusaktif = $result['MEMO'];
+
+            $mandors[$i]['statusaktif'] = $statusaktif;
+
+        
+            $i++;
+
+
+        }
+
+        // dd($gandengans);
+
+        return view('reports.mandor', compact('mandors'));
+    }
 }
