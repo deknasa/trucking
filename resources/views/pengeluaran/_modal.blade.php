5b847fec1b71f7190d1f284ad0e6c799baa284ff
syarifa20 <syaripah2004@gmail.com>
2023-05-25 12:07:43 +0700

    fix diterima ke menjadi not required

--- resources/views/pengeluaran/_modal.blade.php
+++ resources/views/pengeluaran/_modal.blade.php
@@ -62,7 +62,7 @@
             <div class="row form-group">
               <div class="col-12 col-sm-3 col-md-2">
                 <label class="col-form-label">
-                  DIBAYAR KE <span class="text-danger">*</span></label>
+                  DIBAYAR KE
               </div>
               <div class="col-12 col-sm-9 col-md-10">
                 <input type="text" name="dibayarke" class="form-control">
