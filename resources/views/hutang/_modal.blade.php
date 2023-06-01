5f04e4edee0455be563c70bc761039f40c3d3167
Ryanita1402 <ryan_vixy1402@yahoo.com>
2023-06-01 09:54:14 +0700

    perbaikan validasi

--- resources/views/hutang/_modal.blade.php
+++ resources/views/hutang/_modal.blade.php
@@ -316,6 +316,8 @@
       ])
       .then(() => {
         $('#crudModal').modal('show')
+        form.find(`[name="tglbukti"]`).prop('readonly', true)
+        form.find(`[name="tglbukti"]`).parent('.input-group').find('.input-group-append').remove()
       })
       .finally(() => {
         $('.modal-loader').addClass('d-none')
