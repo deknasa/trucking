5c1dd959aede49fbfda8c68bf6ae7a05f774160a
syarifa20 <syaripah2004@gmail.com>
2023-06-12 23:38:31 +0700

    penyesuaian report export

--- resources/views/pelanggan/index.blade.php
+++ resources/views/pelanggan/index.blade.php
@@ -365,6 +365,12 @@
       })
     })
 
+    $('#rangeModal').on('hidden.bs.modal', function() {
+      
+      $('.is-invalid').removeClass('is-invalid')
+      $('.invalid-feedback').remove()
+    })
+
     $('#formRange').submit(function(event) {
       event.preventDefault()
 
@@ -386,6 +392,7 @@
       let limit = parseInt(formRange.find('[name=sampai]').val().replace('.', '')) - offset
       params += `&offset=${offset}&limit=${limit}`
 
+      getCekExport(params).then((response) => {
       if ($('#rangeModal').data('action') == 'export') {
         let xhr = new XMLHttpRequest()
         xhr.open('GET', `{{ config('app.api_url') }}pelanggan/export?${params}`, true)
@@ -409,6 +416,10 @@
           }
         }
 
+        xhr.onerror = () => {
+            submitButton.removeAttr('disabled')
+          }
+
         xhr.send()
       } else if ($('#rangeModal').data('action') == 'report') {
         window.open(`{{ route('pelanggan.report') }}?${params}`)
@@ -416,6 +427,65 @@
         submitButton.removeAttr('disabled')
       }
     })
+    .catch((error) => {
+        if (error.status === 422) {
+          $('.is-invalid').removeClass('is-invalid')
+          $('.invalid-feedback').remove()
+          errors = error.responseJSON.errors
+
+          $.each(errors, (index, error) => {
+            let indexes = index.split(".");
+            indexes[0] = 'sampai'
+            let element;
+            element = $('#rangeModal').find(`[name="${indexes[0]}"]`)[0];
+
+            $(element).addClass("is-invalid");
+            $(`
+              <div class="invalid-feedback">
+              ${error[0].toLowerCase()}
+              </div>
+			    `).appendTo($(element).parent());
+
+          });
+
+          $(".is-invalid").first().focus();
+        } else {
+          showDialog(error.statusText)
+        }
+      })
+      
+      .finally(() => {
+        $('.ui-button').click()
+        
+        submitButton.removeAttr('disabled')
+      })
+    })
+
+    function getCekExport(params) {
+      
+      params += `&cekExport=true`
+
+      return new Promise((resolve, reject) => {
+        $.ajax({
+          url: `${apiUrl}pelanggan/export?${params}`,
+          dataType: "JSON",
+          headers: {
+            Authorization: `Bearer ${accessToken}`
+          },
+          success: (response) => {
+            resolve(response);
+          },
+          error: error => {
+            reject(error)
+
+          },
+        });
+      });
+    }
+
+
+
+
   })
 </script>
 @endpush()
--- resources/views/supplier/index.blade.php
+++ resources/views/supplier/index.blade.php
@@ -444,6 +444,12 @@
       })
     })
 
+    $('#rangeModal').on('hidden.bs.modal', function() {
+      
+      $('.is-invalid').removeClass('is-invalid')
+      $('.invalid-feedback').remove()
+    })
+
     $('#formRange').submit(function(event) {
       event.preventDefault()
 
@@ -465,6 +471,8 @@
       let limit = parseInt(formRange.find('[name=sampai]').val().replace('.', '')) - offset
       params += `&offset=${offset}&limit=${limit}`
 
+
+      getCekExport(params).then((response) => {
       if ($('#rangeModal').data('action') == 'export') {
         let xhr = new XMLHttpRequest()
         xhr.open('GET', `{{ config('app.api_url') }}supplier/export?${params}`, true)
@@ -495,6 +503,64 @@
         submitButton.removeAttr('disabled')
       }
     })
+    .catch((error) => {
+        if (error.status === 422) {
+          $('.is-invalid').removeClass('is-invalid')
+          $('.invalid-feedback').remove()
+          errors = error.responseJSON.errors
+
+          $.each(errors, (index, error) => {
+            let indexes = index.split(".");
+            indexes[0] = 'sampai'
+            let element;
+            element = $('#rangeModal').find(`[name="${indexes[0]}"]`)[0];
+
+            $(element).addClass("is-invalid");
+            $(`
+              <div class="invalid-feedback">
+              ${error[0].toLowerCase()}
+              </div>
+			    `).appendTo($(element).parent());
+
+          });
+
+          $(".is-invalid").first().focus();
+        } else {
+          showDialog(error.statusText)
+        }
+      })
+      
+      .finally(() => {
+        $('.ui-button').click()
+        
+        submitButton.removeAttr('disabled')
+      })
+    })
+
+    
+    function getCekExport(params) {
+      
+      params += `&cekExport=true`
+
+      return new Promise((resolve, reject) => {
+        $.ajax({
+          url: `${apiUrl}supplier/export?${params}`,
+          dataType: "JSON",
+          headers: {
+            Authorization: `Bearer ${accessToken}`
+          },
+          success: (response) => {
+            resolve(response);
+          },
+          error: error => {
+            reject(error)
+
+          },
+        });
+      });
+    }
+
+
   })
 </script>
 @endpush()
