83fdd763a91d9ae2771700ecda22781f8b54273f
syarifa20 <syaripah2004@gmail.com>
2023-06-15 15:05:48 +0700

    hidden id in bank

--- resources/views/bank/_modal.blade.php
+++ resources/views/bank/_modal.blade.php
@@ -10,14 +10,9 @@
         </div>
         <form action="" method="post">
           <div class="modal-body">
-            <div class="row form-group">
-              <div class="col-12 col-md-2">
-                <label class="col-form-label">ID</label>
-              </div>
-              <div class="col-12 col-md-10">
-                <input type="text" name="id" class="form-control" readonly>
-              </div>
-            </div>
+            
+            <input type="hidden" name="id" class="form-control" readonly>
+            
             <div class="row form-group">
               <div class="col-12 col-md-2">
                 <label class="col-form-label">
