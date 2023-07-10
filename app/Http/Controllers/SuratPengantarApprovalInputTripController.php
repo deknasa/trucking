e1a5f47769fdaadee03c1fb394e54ac64cc9bf72
bshf <bungashafa53@gmail.com>
2023-07-10 18:36:01 +0700

    approval surat pengantar inputtrip

--- app/Http/Controllers/SuratPengantarApprovalInputTripController.php
+++ app/Http/Controllers/SuratPengantarApprovalInputTripController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class SuratPengantarApprovalInputTripController extends MyController
 {
@@ -11,6 +12,25 @@ class SuratPengantarApprovalInputTripController extends MyController
     public function index(Request $request)
     {
         $title = $this->title;
-        return view('suratpengantarapprovalinputtrip.index', compact('title'));
+        $data = [
+            'comboapproval' => $this->comboapproval('list', 'STATUS APPROVAL', 'STATUS APPROVAL'),
+        ];
+        return view('suratpengantarapprovalinputtrip.index', compact('title','data'));
+    }
+
+    public function comboapproval($aksi, $grp, $subgrp)
+    {
+        $status = [
+            'status' => $aksi,
+            'grp' => $grp,
+            'subgrp' => $subgrp,
+        ];
+
+        $response = Http::withHeaders($this->httpHeaders)
+            ->withOptions(['verify' => false])
+            ->withToken(session('access_token'))
+            ->get(config('app.api_url') . 'invoiceheader/comboapproval', $status);
+
+        return $response['data'];
     }
 }
