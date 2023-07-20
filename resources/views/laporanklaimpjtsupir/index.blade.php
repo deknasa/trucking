05fa294a960e23ca980e1139a61213bdfd679afe
Ryanita1402 <ryan_vixy1402@yahoo.com>
2023-07-20 13:20:17 +0700

    perbaikan seeder

--- resources/views/laporanklaimpjtsupir/index.blade.php
+++ resources/views/laporanklaimpjtsupir/index.blade.php
@@ -27,8 +27,8 @@
                         <div class="row">
                             <label class="col-12 col-sm-2 col-form-label mt-2">JENIS STOK<span class="text-danger">*</span></label>
                             <div class="col-sm-4 mt-2">
-                                <input type="hidden" name="kategori_id">
-                                <input type="text" name="kategori" class="form-control kategori-lookup">
+                                <input type="hidden" name="kelompok_id">
+                                <input type="text" name="kelompok" class="form-control kelompok-lookup">
                             </div>
                         </div>
                         <div class="row">
@@ -76,7 +76,7 @@
         $('#crudForm').find('[name=sampai]').val($.datepicker.formatDate('dd-mm-yy', new Date())).trigger('change');
 
 
-        
+
         if (!`{{ $myAuth->hasPermission('laporanklaimpjtsupir', 'report') }}`) {
             $('#btnPreview').attr('disabled', 'disabled')
         }
@@ -85,29 +85,63 @@
     $(document).on('click', `#btnPreview`, function(event) {
         let sampai = $('#crudForm').find('[name=sampai]').val()
         let dari = $('#crudForm').find('[name=dari]').val()
-        let kategori = $('#crudForm').find('[name=kategori_id]').val()
+        let kelompok = $('#crudForm').find('[name=kelompok_id]').val()
 
-        if (dari != '' && sampai != '' && kategori != '') {
+        getCekReport().then((response) => {
+            window.open(`{{ route('laporanklaimpjtsupir.report') }}?sampai=${sampai}&dari=${dari}&kelompok=${kelompok}`)
 
-            window.open(`{{ route('laporanklaimpjtsupir.report') }}?sampai=${sampai}&dari=${dari}&kategori=${kategori}`)
-        } else {
-            showDialog('ISI SELURUH KOLOM')
-        }
+        }).catch((error) => {
+            if (error.status === 422) {
+                $('.is-invalid').removeClass('is-invalid')
+                $('.invalid-feedback').remove()
+
+                setErrorMessages($('#crudForm'), error.responseJSON.errors);
+            } else {
+                showDialog(error.statusText, error.responseJSON.message)
+
+            }
+        })
     })
 
 
-    function initLookup()
-    {
-        $('.kategori-lookup').lookup({
-            title: 'Kategori Lookup',
-            fileName: 'kategori',
+    function getCekReport() {
+
+        return new Promise((resolve, reject) => {
+            $.ajax({
+                url: `${apiUrl}laporanklaimpjtsupir/report`,
+                dataType: "JSON",
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                },
+                data: {
+                    sampai: $('#crudForm').find('[name=sampai]').val(),
+                    dari: $('#crudForm').find('[name=dari]').val(),
+                    kelompok: $('#crudForm').find('[name=kelompok_id]').val(),
+                    isCheck: true,
+                },
+                success: (response) => {
+                    resolve(response);
+                },
+                error: error => {
+                    reject(error)
+
+                },
+            });
+        });
+    }
+
+
+    function initLookup() {
+        $('.kelompok-lookup').lookup({
+            title: 'kelompok Lookup',
+            fileName: 'kelompok',
             beforeProcess: function(test) {
                 this.postData = {
                     Aktif: 'AKTIF',
                 }
             },
             onSelectRow: (kategori, element) => {
-                $('#crudForm [name=kategori_id]').first().val(kategori.id)
+                $('#crudForm [name=kelompok_id]').first().val(kategori.id)
                 element.val(kategori.keterangan)
                 element.data('currentValue', element.val())
             },
@@ -116,7 +150,7 @@
             },
             onClear: (element) => {
                 element.val('')
-                $(`#crudForm [name="kategori_id"]`).first().val('')
+                $(`#crudForm [name="kelompok_id"]`).first().val('')
                 element.data('currentValue', element.val())
             }
         })
