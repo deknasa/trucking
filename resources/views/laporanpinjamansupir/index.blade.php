cdf08c0e242162ea41f55bc86b0e2f826d7d6a9d
bshf <bungashafa53@gmail.com>
2023-07-24 15:14:27 +0700

    fix laporan pinjaman supir

--- resources/views/laporanpinjamansupir/index.blade.php
+++ resources/views/laporanpinjamansupir/index.blade.php
@@ -19,6 +19,14 @@
                             </div>
                         </div>
                         <div class="row">
+                            <label class="col-12 col-sm-2 col-form-label mt-2">Jenis Pinjaman<span class="text-danger">*</span></label>
+                            <div class="col-sm-4 mt-2">
+                                <select name="jenis" id="jenis" class="form-select select2bs4" style="width: 100%;">
+
+                                </select>
+                            </div>
+                        </div>
+                        <div class="row">
 
                             <div class="col-sm-6 mt-4">
                                 <button type="button" id="btnPreview" class="btn btn-info mr-1 ">
@@ -56,6 +64,8 @@
 
 
     $(document).ready(function() {
+        initSelect2($('#crudForm').find('[name=jenis]'), false)
+        setJenisKaryawanOptions($('#crudForm'))
 
         initDatepicker()
         $('#crudForm').find('[name=sampai]').val($.datepicker.formatDate('dd-mm-yy', new Date())).trigger('change');
@@ -69,16 +79,18 @@
 
     $(document).on('click', `#btnPreview`, function(event) {
         let sampai = $('#crudForm').find('[name=sampai]').val()
+        let jenis = $('#crudForm').find('[name=jenis]').val()
 
         $.ajax({
             url: `{{ route('laporanpinjamansupir.report') }}`,
             method: 'GET',
             data: {
-                sampai: sampai
+                sampai: sampai,
+                jenis: jenis,
             },
             success: function(response) {
                 // Handle the success response
-                var newWindow = window.open('','_blank');
+                var newWindow = window.open('', '_blank');
                 newWindow.document.open();
                 newWindow.document.write(response);
                 newWindow.document.close();
@@ -94,8 +106,51 @@
                 }
             }
         });
-       
+
     })
+
+
+    const setJenisKaryawanOptions = function(relatedForm) {
+        // return new Promise((resolve, reject) => {
+        // relatedForm.find('[name=approve]').empty()
+        relatedForm.find('[name=jenis]').append(
+            new Option('-- PILIH JENIS PINJAMAN --', '', false, true)
+        ).trigger('change')
+
+        let data = [];
+        data.push({
+            name: 'grp',
+            value: 'STATUS POSTING'
+        })
+        data.push({
+            name: 'subgrp',
+            value: 'STATUS POSTING'
+        })
+        $.ajax({
+            url: `${apiUrl}parameter/combo`,
+            method: 'GET',
+            dataType: 'JSON',
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            },
+            data: data,
+            success: response => {
+
+                response.data.forEach(statusPosting => {
+                    let option = new Option(statusPosting.text, statusPosting.id)
+                    relatedForm.find('[name=jenis]').append(option).trigger('change')
+                });
+
+
+                relatedForm
+                    .find('[name=jenis]')
+                    .val($(`#crudForm [name=jenis] option:eq(1)`).val())
+                    .trigger('change')
+                    .trigger('select2:selected');
+            }
+        })
+        // })
+    }
 </script>
 @endpush()
 @endsection
