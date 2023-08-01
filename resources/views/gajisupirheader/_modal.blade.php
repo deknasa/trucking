552dbac9ec2025ae8f3c06a06dc09e0862084350
bshf <bungashafa53@gmail.com>
2023-08-01 09:56:53 +0700

    update periode aftersubmit gajisupir

--- resources/views/gajisupirheader/_modal.blade.php
+++ resources/views/gajisupirheader/_modal.blade.php
@@ -836,9 +836,17 @@
                     id = response.data.id
                     $('#crudModal').find('#crudForm').trigger('reset')
                     $('#crudModal').modal('hide')
+
+                    $('#rangeHeader').find('[name=tgldariheader]').val(dateFormat(response.data.tgldariheader)).trigger('change');
+                    $('#rangeHeader').find('[name=tglsampaiheader]').val(dateFormat(response.data.tglsampaiheader)).trigger('change');
                     $('#jqGrid').jqGrid('setGridParam', {
-                        page: response.data.page
+                        page: response.data.page,
+                        postData: {
+                            tgldari: dateFormat(response.data.tgldariheader),
+                            tglsampai: dateFormat(response.data.tglsampaiheader)
+                        }
                     }).trigger('reloadGrid');
+
                     selectedRows = []
                     selectedNobukti = [];
                     selectedGajiSupir = [];
