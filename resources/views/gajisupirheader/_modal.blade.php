db446e59c507ed82c1732f2b0c8a60590dda55c9
bshf <bungashafa53@gmail.com>
2023-10-21 09:48:39 +0700

    reload grid after submit gajisupir

--- resources/views/gajisupirheader/_modal.blade.php
+++ resources/views/gajisupirheader/_modal.blade.php
@@ -926,6 +926,7 @@
                     $('#jqGrid').jqGrid('setGridParam', {
                         page: response.data.page,
                         postData: {
+                            proses: 'reload',
                             tgldari: dateFormat(response.data.tgldariheader),
                             tglsampai: dateFormat(response.data.tglsampaiheader)
                         }
