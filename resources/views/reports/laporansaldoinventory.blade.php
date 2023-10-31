4948813591f6618dd5967c1819beefabadcd3306
Ryanita1402 <ryan_vixy1402@yahoo.com>
2023-10-31 14:28:03 +0700

    perbaikan seeder

--- resources/views/reports/laporansaldoinventory.blade.php
+++ resources/views/reports/laporansaldoinventory.blade.php
@@ -46,8 +46,8 @@
 
       report.regData(dataSet.dataSetName, '', dataSet)
       report.dictionary.synchronize()
-      designer.report = report;
-      designer.renderHtml('content');
+      // designer.report = report;
+      // designer.renderHtml('content');
       viewer.report = report
       
      
