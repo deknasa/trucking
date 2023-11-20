b7ed91ee298ba06f7ba703e4df1ecb88dee825c0
bshf <bungashafa53@gmail.com>
2023-11-20 08:46:03 +0700

    lap kartu piutang

--- resources/views/reports/laporankartupiutangperagen.blade.php
+++ resources/views/reports/laporankartupiutangperagen.blade.php
@@ -6,7 +6,7 @@
 <head>
 
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-  <title>Laporan Kartu Piutang Per Agen</title>
+  <title>Laporan Kartu Piutang Per Customer</title>
   <link rel="stylesheet" type="text/css" href="{{ asset($stireport_path . 'css/stimulsoft.viewer.office2013.whiteblue.css') }}">
   <link rel="stylesheet" type="text/css" href="{{ asset($stireport_path . 'css/stimulsoft.designer.office2013.whiteblue.css') }}">
   <script type="text/javascript" src="{{ asset($stireport_path . 'scripts/stimulsoft.reports.js') }}"></script>
@@ -43,8 +43,8 @@
 
       report.regData(dataSet.dataSetName, '', dataSet)
       report.dictionary.synchronize()
-      // designer.report = report;
-      // designer.renderHtml('content');
+      designer.report = report;
+      designer.renderHtml('content');
       viewer.report = report
       
      
