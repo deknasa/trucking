603c76b2e15582a9d1593efa0c61b6cc63e94928
bshf <bungashafa53@gmail.com>
2024-01-15 15:54:35 +0700

    fix lookup di parameter&user

--- resources/views/partials/lookups/parameterMaster.blade.php
+++ resources/views/partials/lookups/parameterMaster.blade.php
@@ -135,7 +135,7 @@ $idLookup = isset($id) ? $id : null;
         fixed: true,
         rownumbers: false,
         rownumWidth: 0,
-        rowNum: `{!! $limit ?? 11 !!}`,
+        rowNum: `{!! $limit ?? 15 !!}`,
         rowList: [10, 20, 50, 0],
         sortable: true,
         sortname: 'id',
