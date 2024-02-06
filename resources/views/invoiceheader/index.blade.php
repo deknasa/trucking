6b07d84827f0b93cc4b0a4fb246e57d1aa74c503
iqbalrafli13 <iqbal13rafli@gmail.com>
2024-02-06 16:11:20 +0700

    tombol apprb

--- resources/views/invoiceheader/index.blade.php
+++ resources/views/invoiceheader/index.blade.php
@@ -722,6 +722,7 @@
         $('#approveun').hide()
         // $('#approval-buka-cetak').attr('disabled', 'disabled')
       }
+      hakApporveCount++
       if (!`{{ $myAuth->hasPermission('invoiceheader', 'approvalbukacetak') }}`) {
         hakApporveCount--
         $('#approval-buka-cetak').hide()
