cdc060827800f7477d00401a6a48364ce4c5cd13
bshf <bungashafa53@gmail.com>
2024-03-23 21:44:19 +0700

    fix pencairangiro

--- resources/views/pindahbuku/index.blade.php
+++ resources/views/pindahbuku/index.blade.php
@@ -630,6 +630,7 @@ function checkboxHandler(element) {
 
   function clearSelectedRows() {
     selectedRows = []
+    selectedbukti = []
 
     $('#gs_').prop('checked', false);
     $('#jqGrid').trigger('reloadGrid')
@@ -650,7 +651,8 @@ function checkboxHandler(element) {
         filters: $('#jqGrid').jqGrid('getGridParam', 'postData').filters
       },
       success: (response) => {
-        selectedRows = response.data.map((penerimaan) => penerimaan.id)
+        selectedRows = response.data.map((datas) => datas.id)
+        selectedbukti =response.data.map((datas) => datas.nobukti)
         $('#jqGrid').trigger('reloadGrid')
       }
     })
