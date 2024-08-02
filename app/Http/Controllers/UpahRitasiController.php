a79a46b4f4da6961d3ce2e895ff7233933ac257b
bshf <bungashafa53@gmail.com>
2024-08-02 15:13:41 +0700

    fix judul laporan upahritasi

--- app/Http/Controllers/UpahRitasiController.php
+++ app/Http/Controllers/UpahRitasiController.php
@@ -197,11 +197,13 @@ class UpahRitasiController extends MyController
             $sheet = $spreadsheet->getActiveSheet();
             $sheet->setCellValue('A1', 'PT. TRANSPORINDO AGUNG SEJAHTERA');
             $sheet->getStyle("A1")->getFont()->setSize(12);
+            $sheet->getStyle("A1")->getFont()->setBold(true);
             $sheet->getStyle('A1')->getAlignment()->setHorizontal('center');
             $sheet->mergeCells('A1:H1');
 
             $sheet->setCellValue('A2', 'Laporan Upah Ritasi');
             $sheet->getStyle("A2")->getFont()->setSize(12);
+            $sheet->getStyle("A2")->getFont()->setBold(true);
             $sheet->getStyle('A2')->getAlignment()->setHorizontal('center');
             $sheet->mergeCells('A2:H2');
 
