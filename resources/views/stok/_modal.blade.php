9c71abd92d0e5a6a583f5f404fa69503c8376906
iqbalrafli13 <iqbal13rafli@gmail.com>
2024-09-02 13:32:08 +0700

    STOK : label requeired di satuan

--- resources/views/stok/_modal.blade.php
+++ resources/views/stok/_modal.blade.php
@@ -117,7 +117,7 @@
 
             <div class="row form-group">
               <div class="col-12 col-sm-3 col-md-2">
-                <label class="col-form-label">satuan </label>
+                <label class="col-form-label">satuan  <span class="text-danger">*</span> </label>
               </div>
               <div class="col-12 col-sm-9 col-md-10">
                 <input type="text" name="satuan" id="satuan" class="form-control satuan-lookup">
