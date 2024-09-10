987560c6ac8693a0f9a04a5800bac3d260bf4cf3
ryan ryan <ryan_vixy1402@yahoo.com>
2024-09-11 05:49:31 +0700

    perbaikan surat pengantar

--- resources/views/partials/lookups/jenisorderMaster.blade.php
+++ resources/views/partials/lookups/jenisorderMaster.blade.php
@@ -61,105 +61,12 @@ $idLookup = isset($id) ? $id : null;
                 sortable: false,
                 search: false,
             },
-            {
-                label: 'JENIS ORDER',
-                name: 'kodejenisorder',
-            },
+           
             {
                 label: 'KETERANGAN',
                 name: 'keterangan',
             },
-            {
-                label: 'STATUS AKTIF',
-                name: 'statusaktif',
-                stype: 'select',
-                searchoptions: {
-                    dataInit: function(element) {
-                        $(element).select2({
-                            width: 'resolve',
-                            theme: "bootstrap4",
-                            ajax: {
-                                url: `${apiUrl}parameter/combo`,
-                                dataType: 'JSON',
-                                headers: {
-                                    Authorization: `Bearer ${accessToken}`
-                                },
-                                data: {
-                                    grp: 'STATUS AKTIF',
-                                    subgrp: 'STATUS AKTIF'
-                                },
-                                beforeSend: () => {
-                                    // clear options
-                                    $(element).data('select2').$results.children().filter((index,
-                                        element) => {
-                                        // clear options except index 0, which
-                                        // is the "searching..." label
-                                        if (index > 0) {
-                                            element.remove()
-                                        }
-                                    })
-                                },
-                                processResults: (response) => {
-                                    let formattedResponse = response.data.map(row => ({
-                                        id: row.text,
-                                        text: row.text
-                                    }));
-
-                                    formattedResponse.unshift({
-                                        id: '',
-                                        text: 'ALL'
-                                    });
-
-                                    return {
-                                        results: formattedResponse
-                                    };
-                                },
-                            }
-                        });
-                    }
-                },
-                formatter: (value, options, rowData) => {
-                    let statusAktif = JSON.parse(value)
-
-                    let formattedValue = $(`
-                <div class="badge" style="background-color: ${statusAktif.WARNA}; color: ${statusAktif.WARNATULISAN};">
-                  <span>${statusAktif.SINGKATAN}</span>
-                </div>
-              `)
-
-                    return formattedValue[0].outerHTML
-                },
-                cellattr: (rowId, value, rowObject) => {
-                    let statusAktif = JSON.parse(rowObject.statusaktif)
-
-                    return ` title="${statusAktif.MEMO}"`
-                }
-            },
-            {
-                label: 'MODIFIED BY',
-                name: 'modifiedby',
-                align: 'left'
-            },
-            {
-                label: 'CREATED AT',
-                name: 'created_at',
-                align: 'right',
-                formatter: "date",
-                formatoptions: {
-                    srcformat: "ISO8601Long",
-                    newformat: "d-m-Y H:i:s"
-                }
-            },
-            {
-                label: 'UPDATED AT',
-                name: 'updated_at',
-                align: 'right',
-                formatter: "date",
-                formatoptions: {
-                    srcformat: "ISO8601Long",
-                    newformat: "d-m-Y H:i:s"
-                }
-            },
+         
 
         ]
     }
